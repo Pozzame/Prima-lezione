5b9d31b943c6466a277ff81d2e99f7a573261d70
Pozzame <pozzame@gmail.com>
2024-06-19 15:30:34 +0200

    Consolle

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello Worlde!!!");
+﻿Console.WriteLine("Scrivi qualcosa");
+string a = Console.ReadLine()!;
+Console.WriteLine("Hai scritto: " + a);
+
+Console.WriteLine("Con che tasto vuoi uscire?");
+ConsoleKeyInfo c = Console.ReadKey();
+Console.WriteLine("\nHai scelto: " + c.KeyChar + ". Prova! :-D");
+
+while (Console.ReadKey() != c) {}
