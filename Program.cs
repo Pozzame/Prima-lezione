37932ea19533b2adba11df895ecf323517ca3976
Pozzame <pozzame@gmail.com>
2024-06-19 15:37:27 +0200

    Interpolazione stringhe

--- Program.cs
+++ Program.cs
@@ -1,9 +1,15 @@
 ﻿Console.WriteLine("Scrivi qualcosa");
 string a = Console.ReadLine()!;
-Console.WriteLine("Hai scritto: " + a);
+Console.WriteLine("Come ti chiami di nome?");
+string nome = Console.ReadLine()!;
+Console.WriteLine("Cognome?");
+string cognome = Console.ReadLine()!;
+Console.WriteLine("Soprannome?");
+string soprannome = Console.ReadLine()!;
+Console.WriteLine($"{nome} {soprannome} {cognome} Hai scritto: " + a);
 
 Console.WriteLine("Con che tasto vuoi uscire?");
 ConsoleKeyInfo c = Console.ReadKey();
-Console.WriteLine("\nHai scelto: " + c.KeyChar + ". Prova! :-D");
+Console.WriteLine("\nHai scelto: " + c.KeyChar + ". Provalo! :-D");
 
 while (Console.ReadKey() != c) {}
