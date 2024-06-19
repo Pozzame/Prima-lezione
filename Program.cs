281e6405705da98354809506a94abff22d75e851
Pozzame <pozzame@gmail.com>
2024-06-19 15:57:12 +0200

    Commenti

--- Program.cs
+++ Program.cs
@@ -1,15 +1,15 @@
 ﻿Console.WriteLine("Scrivi qualcosa");
-string a = Console.ReadLine()!;
+string a = Console.ReadLine()!; //null-forgiving sopprime il warning
 Console.WriteLine("Come ti chiami di nome?");
-string? nome = Console.ReadLine();
+string? nome = Console.ReadLine(); //make nullable a non-nullable type
 Console.WriteLine("Cognome?");
 string cognome = Console.ReadLine()!;
 Console.WriteLine("Soprannome?");
 string soprannome = Console.ReadLine()!;
-Console.WriteLine($"{nome} {soprannome} {cognome} Hai scritto: " + a);
+Console.WriteLine($"{nome} {soprannome} {cognome} Hai scritto: " + a); //Interpolazione e concatenazione stringhe
 
 Console.WriteLine("Con che tasto vuoi uscire?");
-ConsoleKeyInfo c = Console.ReadKey(true);
+ConsoleKeyInfo c = Console.ReadKey(true); //hide carattere premuto
 Console.WriteLine("\nHai scelto: " + c.KeyChar + ". Provalo! :-D");
 
 while (Console.ReadKey() != c) {}
