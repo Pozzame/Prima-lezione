72d8db913039d9d0d2be4c011113a9ee46879eb4
Pozzame <pozzame@gmail.com>
2024-06-21 14:44:49 +0200

    Switch

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.Clear();
-/*Console.WriteLine("Scrivi qualcosa");
+Console.WriteLine("Scrivi qualcosa");
 string a = Console.ReadLine()!; //null-forgiving sopprime il warning
 Console.WriteLine("Come ti chiami di nome?");
 string? nome = Console.ReadLine(); //make nullable a non-nullable type
@@ -8,9 +8,45 @@ string? cognome = Console.ReadLine();
 Console.WriteLine("Soprannome?");
 string? soprannome = Console.ReadLine();
 Console.WriteLine($"{nome} {soprannome} {cognome} Hai scritto: " + a); //Interpolazione e concatenazione stringhe
-*/
+
 int num = 12;
-Console.WriteLine("{0:D3}", num);
+int ver = 13;
+
+if (num == ver){
+    Console.Write("{0:D3}", num);
+    Console.WriteLine(" è uguale a " + ver);}
+else if (num < ver)
+    Console.WriteLine($"{num} è minore di {ver}");
+else if (num > ver)
+    Console.WriteLine($"{num} è maggiore di {ver}");
+
+switch (num) {
+    case 1:
+        Console.WriteLine("Lunedì");
+        break;
+    case 2:
+        Console.WriteLine("Martedì");
+        break;
+    case 3:
+        Console.WriteLine("Mercoledì");
+        break;
+    case 4:
+        Console.WriteLine("Giovedì");
+        break;
+    case 5:
+        Console.WriteLine("Venerdì");
+        break;
+    case 6:
+        Console.WriteLine("Sabato");
+        break;
+    case 7:
+        Console.WriteLine("Domenica");
+        break;
+    default:
+        Console.WriteLine("Non è un giorno della settimana.");
+        break;
+}
+
 
 Console.WriteLine("Con che tasto vuoi uscire?");
 ConsoleKeyInfo c = Console.ReadKey(true); //hide carattere premuto
