2f62c33c44a46e886f5fc9465768ee7e884c50fb
Pozzame <pozzame@gmail.com>
2024-06-21 10:13:00 +0200

    Finale

--- Program.cs
+++ Program.cs
@@ -3,9 +3,9 @@ string a = Console.ReadLine()!; //null-forgiving sopprime il warning
 Console.WriteLine("Come ti chiami di nome?");
 string? nome = Console.ReadLine(); //make nullable a non-nullable type
 Console.WriteLine("Cognome?");
-string cognome = Console.ReadLine()!;
+string? cognome = Console.ReadLine();
 Console.WriteLine("Soprannome?");
-string soprannome = Console.ReadLine()!;
+string? soprannome = Console.ReadLine();
 Console.WriteLine($"{nome} {soprannome} {cognome} Hai scritto: " + a); //Interpolazione e concatenazione stringhe
 
 Console.WriteLine("Con che tasto vuoi uscire?");
