c128d30eb870aaffbd9a408c8676c219ad60c562
Pozzame <pozzame@gmail.com>
2024-06-21 12:57:55 +0200

    Zeri

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.Clear();
-Console.WriteLine("Scrivi qualcosa");
+/*Console.WriteLine("Scrivi qualcosa");
 string a = Console.ReadLine()!; //null-forgiving sopprime il warning
 Console.WriteLine("Come ti chiami di nome?");
 string? nome = Console.ReadLine(); //make nullable a non-nullable type
@@ -8,9 +8,12 @@ string? cognome = Console.ReadLine();
 Console.WriteLine("Soprannome?");
 string? soprannome = Console.ReadLine();
 Console.WriteLine($"{nome} {soprannome} {cognome} Hai scritto: " + a); //Interpolazione e concatenazione stringhe
+*/
+int num = 12;
+Console.WriteLine("{0:D3}", num);
 
 Console.WriteLine("Con che tasto vuoi uscire?");
 ConsoleKeyInfo c = Console.ReadKey(true); //hide carattere premuto
 Console.WriteLine("\nHai scelto: " + c.KeyChar + ". Provalo! :-D");
 
-while (Console.ReadKey() != c) {}
+while (Console.ReadKey() != c) { }
